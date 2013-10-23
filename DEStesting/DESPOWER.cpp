1f020d28a6812f6938266cbceadb931fcaa7f392
parastuffs <parastuffs@gmail.com>
2013-10-23 11:09:51 +0200

    DES working. See repository parastuffs/CryptoPP_DES.

--- DEStesting/DESPOWER.cpp
+++ DEStesting/DESPOWER.cpp
@@ -8,6 +8,7 @@
 
 #include <iomanip>
 #include <cstdio>
+#include <bitset>
 
 using namespace std;
 
@@ -15,10 +16,27 @@ const int PASS_NBR_BITS = 12;
 
 int main(int argc, char* argv[])
 {
-	 
+/*	 
 	//Code from codeproject
-	byte plainText[] = {'H','E','L','L','O',' ','W','O','R','L','D',
-		0x0,0x0,0x0,0x0,0x0};
+	//byte plainText[] = {'H','E','L','L','O',' ','W','O','R','L','D',
+	//	0x0,0x0,0x0,0x0,0x0};
+	
+	//Il se peut que j'aie un probleme avec le fait que byte n'est pas un tableau
+	//de bytes (de 8 bits). Il faudrait alors que je me démerde pour parcourir le
+	//bitset et le couper en paquets de 8 bits. Utiliser sizeof(bitset) ?
+	bitset<12> password(string("101010101010"));
+	//bitset<16> extendedPass(string("0100000101000010"));//Test with "0000" concatenated with password.to_string
+	bitset<8> passA(string("01000001"));
+	bitset<8> passB(string("01000010"));
+	//byte plainText[] = {passA.to_ulong(),passB.to_ulong()};
+	byte plainText[] = {passA.to_ulong()};
+
+	//
+	//Trying to convert 56btits long bitset into a table of bytes
+	//
+	bitset<56> extPass
+
+
 	byte key[CryptoPP::DES::DEFAULT_KEYLENGTH];
 	::memset(key,0x01,CryptoPP::DES::DEFAULT_KEYLENGTH);
 	
@@ -42,16 +60,41 @@ int main(int argc, char* argv[])
 	cout << "Recovered text(" << sizeof(cbRecoveredText) << " bytes): ";
 	cout << "'" << cbRecoveredText << "'" << endl;
 
+	cout << "### Passing a BITESET to a BYTE" << endl;
+
+	bitset<8> word(string("01000001"));//A
+	byte wordByte = word.to_ulong();
+	cout << "word: '" << word << "'" << endl;
+	cout << "wordByte: '" << wordByte << "'" << endl;
+
+	cout << "### Trying to pass a BYTE to a BITSET" << endl;
+	byte secondWord = 'B';
+	bitset<8> secondBitset = secondWord;
+	cout << "secondWord: '" << secondWord << "'" << endl;
+	cout << "secondBitset: '" << secondBitset << "'" << endl;
+	
+*/
 	//Code from the cryptoPP's wiki
-/*
+
 	CryptoPP::AutoSeededRandomPool prng;
 	byte key[CryptoPP::DES::DEFAULT_KEYLENGTH];
 	prng.GenerateBlock(key,sizeof(key));
 
 	string plain = "ECB Mode Test";
 	string cipher, encoded;
+	string decoded;
+
+	//Conversion du plaintext en hex
+	CryptoPP::StringSource(plain,true,
+		new CryptoPP::HexEncoder(
+			new CryptoPP::StringSink(encoded)
+		)
+	);
+	cout << "Plain Text Hex Style: " << encoded << endl;
+
 
 	encoded.clear();
+	//conversion de la clé en hexadécimal
 	CryptoPP::StringSource(key,sizeof(key),true,
 		new CryptoPP::HexEncoder(
 			new CryptoPP::StringSink(encoded)
@@ -64,6 +107,7 @@ int main(int argc, char* argv[])
 	CryptoPP::ECB_Mode<CryptoPP::DES>::Encryption e;
 	e.SetKey(key,sizeof(key));
 
+	//Chiffrement du plaintext
 	CryptoPP::StringSource(plain,true,
 		new CryptoPP::StreamTransformationFilter(e,
 			new CryptoPP::StringSink(cipher)
@@ -71,13 +115,31 @@ int main(int argc, char* argv[])
 	);
 
 	encoded.clear();
+	//Conversion du texte chiffré en hexadécimal
 	CryptoPP::StringSource(cipher,true,
 		new CryptoPP::HexEncoder(
 			new CryptoPP::StringSink(encoded)
 		)
 	);
 	cout << "Cipher Text: " << encoded << endl;
-*/
+	
+	CryptoPP::ECB_Mode<CryptoPP::DES>::Decryption d;
+	d.SetKey(key,sizeof(key));
+	decoded.clear();
+	CryptoPP::StringSource(cipher,true,
+		new CryptoPP::StreamTransformationFilter(d,
+			new CryptoPP::StringSink(decoded)
+		)
+	);
+
+	encoded.clear();
+	CryptoPP::StringSource(decoded,true,
+		new CryptoPP::HexEncoder(
+			new CryptoPP::StringSink(encoded)
+		)
+	);
+	cout << "Decoded Text: " << encoded << endl;
+
 
 
 	return 0;
